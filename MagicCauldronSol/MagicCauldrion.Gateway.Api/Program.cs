fbf2a1f660bb129447ec54adff32a1bbce1a4b49
Stalker <vihovaneckostik@gmil.com>
2025-02-06 14:03:22 +0200

    add configuration file for microservices add possibility for swagger (but not tested)

--- MagicCauldronSol/MagicCauldrion.Gateway.Api/Program.cs
+++ MagicCauldronSol/MagicCauldrion.Gateway.Api/Program.cs
@@ -1,12 +1,27 @@
+using MMLib.Ocelot.Provider.AppConfiguration;
+using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+#region Ocelot
+builder.Configuration.AddOcelotWithSwaggerSupport(options =>
+{
+    options.Folder = "OcelotConfiguration";
+});
+builder.Services.AddOcelot(builder.Configuration).AddAppConfiguration();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+#endregion
+
+
 builder.Services.AddOpenApi();
 builder.Configuration.AddJsonFile($"ocelot.json", false ,true);
 
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -15,9 +30,15 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+#region Ocelot
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+
+});
+app.UseWebSockets();
+app.UseOcelot().Wait(); 
+#endregion
 
 app.UseHttpsRedirection();
-
-await app.UseOcelot();
-
 app.Run();
